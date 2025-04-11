b20df5bf45733c98ac4f5c8b744476055f0a7114
Aryan <arynch24@gmail.com>
2025-04-11 20:33:27 +0530

    Update error handling and code coverage

--- week-3/01-middlewares/03-errorcount.js
+++ week-3/01-middlewares/03-errorcount.js
@@ -10,17 +10,36 @@ let errorCount = 0;
 // 1. Ensure that if there is ever an exception, the end user sees a status code of 404
 // 2. Maintain the errorCount variable whose value should go up every time there is an exception in any endpoint
 
-app.get('/user', function(req, res) {
-  throw new Error("User not found");
-  res.status(200).json({ name: 'john' });
+app.get('/user', function (req, res) {
+  try {
+    throw new Error("User not found");
+    res.status(200).json({ name: 'john' });
+  }
+  catch (err) {
+    errorCount++;
+    res.status(404).send(err);
+  }
+
 });
 
-app.post('/user', function(req, res) {
-  res.status(200).json({ msg: 'created dummy user' });
+app.post('/user', function (req, res) {
+  try {
+    res.status(200).json({ msg: 'created dummy user' });
+  }
+  catch (err) {
+    errorCount++;
+    res.status(404).send(err);
+  }
 });
 
-app.get('/errorCount', function(req, res) {
-  res.status(200).json({ errorCount });
+app.get('/errorCount', function (req, res) {
+  try {
+    res.status(200).json({ errorCount });
+  }
+  catch (err) {
+    errorCount++;
+    res.status(404).send(err);
+  }
 });
 
 module.exports = app;
