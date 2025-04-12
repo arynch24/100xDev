9faf22f3a4a91a1fab211122882e43910fd228f5
Aryan <arynch24@gmail.com>
2025-04-12 19:40:21 +0530

    Update dependencies and implement JWT authentication

--- week-3/04-mongo-with-jwt-auth/db/index.js
+++ week-3/04-mongo-with-jwt-auth/db/index.js
@@ -1,20 +1,35 @@
 const mongoose = require('mongoose');
 
 // Connect to MongoDB
-mongoose.connect('your-mongodb-url');
+mongoose.connect('mongodb+srv://arynch24:acuPBrkfuQK3BgPe@cluster-100xdev.6ifyamk.mongodb.net/Course_Selling_App');
 
-// Define schemas
+// Define schemas //
+
 const AdminSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String
 });
 
 const UserSchema = new mongoose.Schema({
     // Schema definition here
+    username: String,
+    password: String,
+    purchasedCourse: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }]
 });
 
 const CourseSchema = new mongoose.Schema({
     // Schema definition here
+    title: String,
+    description: String,
+    imageLink: String,
+    price: Number
 });
+
+//Defining Models to do CRUD on Table
 
 const Admin = mongoose.model('Admin', AdminSchema);
 const User = mongoose.model('User', UserSchema);
--- week-3/04-mongo-with-jwt-auth/middleware/user.js
+++ week-3/04-mongo-with-jwt-auth/middleware/user.js
@@ -1,6 +1,29 @@
+const jwt = require('jsonwebtoken');
+const jwtPassword = "hulalala";
+
 function userMiddleware(req, res, next) {
-    // Implement user auth logic
-    // You need to check the headers and validate the user from the user DB. Check readme for the exact headers to be expected
+    // Implement admin auth logic
+    // You need to check the headers and validate the admin from the admin DB. Check readme for the exact headers to be expected
+
+    const token = req.headers.authorization;
+    const words = token.split(" ");
+    const jwtToken = words[1];
+
+    try {
+        const user = jwt.verify(jwtToken, jwtPassword);
+
+        if (user.username) {
+            next();
+        }
+        else {
+            res.send("You are not authenticated");
+        }
+    }
+    catch{
+        res.json({
+            message:"Invalid user details."
+        })
+    }
 }
 
 module.exports = userMiddleware;
